db0540a547bd1f0f25c6d3a046249c09ef402db9
Miguel Angel Useche Castro <migueluseche@skatox.com>
2015-11-29 00:12:19 -0430

    Arreglando validación de correo

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function FormContacto() {
+    var str, ruta=document.Contacto, error="";
+    if(ruta.inputName.value == "")
+    { error += "Debe ingresar su nombre \n";}
+
+    if(ruta.inputEmail.value == "")
+    { error += "Debe ingresar su correo electronico \n";}
+    if(ruta.inputEmail.value != ""){ str=ruta.inputEmail.value;
+       if(!str.match(/^[\w]{1}[\w\.\-_]*@[\w]{1}[\w\-_\.]*\.[\w]{2,6}$/i)){
+       error += "Formato email invalido\n"; }}
+    if(ruta.inputMessage.value == "")
+    { error += "Debe escribir un mensaje \n";}
+    if(error!=""){ alert("Lista de Errores encontrados:\n\n"+error); return false; }
+}
